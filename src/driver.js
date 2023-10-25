4b1914f15303a760af7e67d3d1859cfcc686befd
Ray <rayoneill.97@gmail.com>
2023-10-25 14:24:45 -0400

    Enforced style guide via ESLint; fixed style issues in driver.js;

--- src/driver.js
+++ src/driver.js
@@ -1,10 +1,9 @@
-import { TodoItem } from "./classes/TodoItem";
-import { Priority } from "./util/Priority";
-import { format, parseISO } from "date-fns";
-import { todoMaker } from "./util/TodoMaker";
+import { TodoItem } from './classes/TodoItem';
+import { Priority } from './util/Priority';
+import { format, parseISO } from 'date-fns';
+import { todoMaker } from './util/TodoMaker';
 import '../res/styles/main-style.css';
-
-const _ = require('lodash');
+import { _ } from 'lodash';
 
 const titleField = document.getElementById('title-field');
 const dateField = document.getElementById('date-field');
@@ -18,7 +17,7 @@ document.querySelector('#create-todo-form').addEventListener('submit', (e) => {
     const title = titleField.value;
     const date = format(parseISO(dateField.value), 'MM/dd/yyyy');
     const notes = notesField.value;
-    const prio = _.find(radios, function(item) { return item.checked === true }).value; // get checked priority's value
+    const prio = _.find(radios, function(item) { return item.checked === true; }).value; // get checked priority's value
     
     const todoItem = todoMaker.makeItem(title, date, notes, prio);
     todoItem.printInfo();
